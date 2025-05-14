2615d4e8de710b46bd2dca749f1532f513b939db
Abdohegazy3 <93677036+Abdohegazy3@users.noreply.github.com>
2025-05-14 04:51:53 +0300

    Update shouldCompress.js

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,21 +1,13 @@
-const MIN_COMPRESS_LENGTH = 1024
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100
+const MIN_COMPRESS_LENGTH = 1024;
+const MIN_TRANSPARENT_COMPRESS_LENGTH = 102400;
 
-function shouldCompress(req) {
-  const { originType, originSize, jpeg } = req.params
-
-  if (!originType.startsWith('image')) return false
-  if (originSize === 0) return false
-  if (jpeg && originSize < MIN_COMPRESS_LENGTH) return false
-  if (
-    !jpeg &&
-    (originType.endsWith('png') || originType.endsWith('gif')) &&
-    originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
-  ) {
-    return false
-  }
-
-  return true
+function shouldCompress(imageType, size, isTransparent) {
+  return !(
+    !imageType.startsWith("image") ||
+    size === 0 ||
+    (isTransparent && size < MIN_COMPRESS_LENGTH) ||
+    (!isTransparent && (imageType.endsWith("png") || imageType.endsWith("gif")) && size < MIN_TRANSPARENT_COMPRESS_LENGTH)
+  );
 }
 
-module.exports = shouldCompress
+module.exports = shouldCompress;
